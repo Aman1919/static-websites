47776e77eeb80df0c0baf0cd68cd51297571db2e
Aman1919 <amansingh33849@gmail.com>
2023-01-06 18:34:26 +0530

    added localStorage to to-do-list-app

--- To do list/app.js
+++ To do list/app.js
@@ -1,4 +1,6 @@
-// diff  btw append it takes both string and it doesn't return any value but node object but appendChild takes only node obj and return the node object
+// Mostly I am making the website with javascirpt for practicing
+// i have made a adder function so that i can reuse that it has paramerters like this adder(parentElement,childElement,element,Class,its innerHtml)
+
 let contanier = document.getElementById("contanier");
 
 let h1 = adder(contanier, "h1", "header", "TO DO List");
@@ -8,8 +10,16 @@ inptDiv.setAttribute["required"];
 let inpt = adder(inptDiv, "input", "inpt", "inpt");
 let btn = adder(inptDiv, "button", "submit", "Add Task");
 let listEl = adder(content, "div", "list", "<h2>Tasks to do</h2>");
-let dele = document.querySelectorAll("button.delete");
 let tasks = [];
+
+let ta = JSON.parse(localStorage.getItem("tasks")); //Every time the page reloads it is getting tasks from the localstorage
+
+if (ta === null) {
+  //if the list is empty
+  tasks = [];
+} else {
+  tasks = ta;
+}
 
 btn.addEventListener("click", submit);
 function submit() {
@@ -17,25 +27,29 @@ function submit() {
     alert("enter Some thing");
   } else {
     tasks.push(inpt.value);
-    task(tasks[tasks.length - 1]);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    // task(inpt.value);
-    console.log(tasks);
-    console.log(dele);
+    localStorage.setItem("tasks", JSON.stringify(tasks)); //Updating every time the localStorage after clicking
 
     inpt.value = "";
   }
+  task();
 }
 
-function task(t) {
-  let listEl = document.querySelector(".list");
-  let taskel = adder(listEl, "div", "task", "");
-  let taskInpt = adder(taskel, "input", "inpt", "");
-  taskInpt.setAttribute("readonly", "true");
-  taskInpt.value = t;
-  let deletetask = adder(taskel, "button", "delete", "Delete");
-  deletetask.addEventListener("click", (e) => {
-    listEl.removeChild(taskel);
+task();
+
+function task() {
+  listEl.innerHTML = "";
+  // Displaying the content of localstorage
+  tasks.forEach((t, index) => {
+    let taskel = adder(listEl, "div", "task", "");
+    let taskInpt = adder(taskel, "input", "inpt", "");
+    taskInpt.setAttribute("readonly", "true");
+    taskInpt.value = t;
+    let deletetask = adder(taskel, "button", "delete", "Delete");
+    deletetask.addEventListener("click", (e) => {
+      listEl.removeChild(taskel);
+      tasks.splice(index, 1);
+      localStorage.setItem("tasks", JSON.stringify(tasks));
+    });
   });
 }
 
